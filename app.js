4f7cab60bbf963d6bae2c106a66cb15f07988dde
Gen Yap Feng Yuan <58755531+TheKinng96@users.noreply.github.com>
2020-05-22 12:24:08 +0900

    added a minigrid to show the up coming tetromino

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ document.addEventListener('DOMContentLoaded', () => {
   const scoreDisplay = document.getElementById('score')
   const startBtn = document.getElementById('startBtn')
   const width = 10
+  let nextRandom = 0
 
   const lTetromino = [
     [1, width + 1, width * 2 + 1, 2],
@@ -72,11 +73,11 @@ document.addEventListener('DOMContentLoaded', () => {
     if (e.keyCode === 37) {
       moveLeft()
     } else if (e.keyCode === 38) {
-      // rotate()
+      rotate()
     } else if (e.keyCode === 39) {
       moveRight()
     } else if (e.keyCode === 40) {
-      // moveDown()
+      moveDown()
     }
   }
   document.addEventListener('keyup',control)
@@ -94,10 +95,12 @@ document.addEventListener('DOMContentLoaded', () => {
     if ( current.some(index => squares[currentPosition + index + width].classList.contains('taken'))) {
       current.forEach(index => squares[currentPosition + index].classList.add('taken'))
       // start a new tetromino
-      random = Math.floor(Math.random() * theTetrominoes.length)
+      random = nextRandom
+      nextRandom = Math.floor(Math.random() * theTetrominoes.length)
       current = theTetrominoes[random][currentRotation]
       currentPosition = 4
       draw()
+      displayShape()
     }
   }
 
@@ -129,9 +132,41 @@ document.addEventListener('DOMContentLoaded', () => {
     draw()
   }
 
+  // rotate the tetromino
+  function rotate () {
+    undraw()
+    currentRotation++
+    if (currentRotation === current.length) {
+      currentRotation = 0
+    }
+    current = theTetrominoes[random][currentRotation]
+    draw()
+  }
 
+  // show up next tetromino in minigrid display
+  const displaySquares = document.querySelectorAll('.mini-grid div')
+  const displayWidth = 4
+  let displayIndex = 0
 
+  // the tetrominos without rotation
+  const upNextTetrominoes = [
+    [1, displayWidth + 1, displayWidth * 2 + 1, 2],
+    [0, displayWidth, displayWidth + 1, displayWidth * 2 + 1],
+    [1, displayWidth, displayWidth + 1, displayWidth + 2],
+    [0, 1, displayWidth, displayWidth + 1],
+    [1, displayWidth + 1, displayWidth * 2 + 1, displayWidth * 3 + 1]
+  ]
 
+  // display the shape in the minigrid
+  function displayShape () {
+    // remove any trace of a tetromino from the entire grid
+    displaySquares.forEach(square => {
+      square.classList.remove('tetromino')
+    })
+    upNextTetrominoes[nextRandom].forEach(index => {
+      displaySquares[displayIndex + index].classList.add('tetromino')
+    })
+  }
 
 
 
